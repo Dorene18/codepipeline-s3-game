8dc1b45c3ed17d8b54087a5109c939379242611e
Dorene18 <149309373+Dorene18@users.noreply.github.com>
2024-05-07 22:09:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,24 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'card10', img: 'images/10.png' },
         // ...add more pairs as needed
     ];
+    let currentFlashcardIndex = 0;
+    function createBoard() {
+        displayFlashcard(currentFlashcardIndex);
+        startTimer();
+        shuffle(cardArray);
+        grid.innerHTML = '';
+        cardsWon = [];
+
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', flipCard);
+            grid.appendChild(card);
+        }
+    }
+
+    
      function startTimer() {
         const countdown = setInterval(() => {
             timeLeft--;
@@ -46,21 +64,7 @@ startButton.addEventListener('click', () => {
         array.sort(() => 0.5 - Math.random());
     }
 
-    function createBoard() {
-        startTimer();
-        shuffle(cardArray);
-        grid.innerHTML = '';
-        cardsWon = [];
-
-        for (let i = 0; i < cardArray.length; i++) {
-            const card = document.createElement('img');
-            card.setAttribute('src', 'images/blank.png');
-            card.setAttribute('data-id', i);
-            card.addEventListener('click', flipCard);
-            grid.appendChild(card);
-        }
-    }
-
+    
     function flipCard() {
         let cardId = this.getAttribute('data-id');
         if (!cardsChosenId.includes(cardId)) {
@@ -73,29 +77,7 @@ startButton.addEventListener('click', () => {
         }
     }
 
-    function checkForMatch() {
-        const cards = document.querySelectorAll('#game-board img');
-        const firstCardId = cardsChosenId[0];
-        const secondCardId = cardsChosenId[1];
-
-        if (cardsChosen[0] === cardsChosen[1] && firstCardId !== secondCardId) {
-            cards[firstCardId].style.visibility = 'hidden';
-            cards[secondCardId].style.visibility = 'hidden';
-            cards[firstCardId].removeEventListener('click', flipCard);
-            cards[secondCardId].removeEventListener('click', flipCard);
-            cardsWon.push(cardsChosen);
-        } else {
-            cards[firstCardId].setAttribute('src', 'images/blank.png');
-            cards[secondCardId].setAttribute('src', 'images/blank.png');
-        }
-
-        cardsChosen = [];
-        cardsChosenId = [];
-
-        if (cardsWon.length === cardArray.length / 2) {
-            alert('Congratulations! You found them all!');
-        }
-    }
+    
 
     startButton.addEventListener('click', createBoard);
 });
