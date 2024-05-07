d70373f39c95c3310197580338f7bdb01700a981
Dorene18 <149309373+Dorene18@users.noreply.github.com>
2024-05-07 20:41:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'card2', img: 'images/distracted.png' },
         { name: 'card2', img: 'images/drake.png' },
         { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
+        { name: 'card5', img: 'images/5.jpeg' },
         { name: 'card6', img: 'images/6.png' },
         { name: 'card7', img: 'images/7.png' },
         { name: 'card8', img: 'images/8.jpeg' },
